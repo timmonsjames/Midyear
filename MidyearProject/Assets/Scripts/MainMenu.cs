69b5a6336d0bfa5265494c83f07193116eb30cfc
RenLeaf1 <1508kb5@gmail.com>
2026-01-08 02:13:57 -0500

    Start Screen UI
    Added two functional buttons:
    Start & Quit
    
    Instructions button to be finished

---
+++ MidyearProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void GoToScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp() {
+        Debug.Log("Quit game requested");
+        Application.Quit();
+    }
+}
+    
